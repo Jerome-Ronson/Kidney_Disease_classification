62ebf22deecfb5ae9ed83bcbf6497085789fedd7
Jerome Ronson Paul <jronsonpaul@gamil.com>
2023-11-26 13:25:07 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from cnn_Classifier import logger
+from cnn_Classifier.pipeline.stag_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to custon log ")
+
+
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} Started <<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STAGE_NAME} completed <<<< \n\nX=====X")
+except Exception as e:
+    logger.exception(e)
+    raise 
