117c18cc758a1c7312cacb5c01ecedb448bfa85d
Jerome Ronson Paul <jronsonpaul@gamil.com>
2023-11-29 00:03:32 +0530

    Base Model Prepration added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnn_Classifier import logger
-from cnn_Classifier.pipeline.stag_01_data_ingestion import DataIngestionTrainingPipeline
+from cnn_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnn_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -14,3 +15,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise 
+
+
+
+
+STAGE_NAME = "Preapare base model"
+try:
+        logger.info(f">>>> stage {STAGE_NAME} Started <<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>> Stage {STAGE_NAME} completed <<<< \n\nX=====X")
+except Exception as e:
+    logger.exception(e)
+    raise e
