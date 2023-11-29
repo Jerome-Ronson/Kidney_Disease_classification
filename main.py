17da9d20d7a15e1e8a3e53ef9d3fd5a5096cb997
Jerome Ronson Paul <jronsonpaul@gamil.com>
2023-11-29 20:55:37 +0530

    Trained the model

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from cnn_Classifier import logger
 from cnn_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnn_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
-
+from cnn_Classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 
@@ -17,14 +16,24 @@ except Exception as e:
     raise 
 
 
-
-
-STAGE_NAME = "Preapare base model"
+STAGE_NAME = "Prepare base model"
 try:
-        logger.info(f">>>> stage {STAGE_NAME} Started <<<<")
-        obj = PrepareBaseModelTrainingPipeline()
-        obj.main()
-        logger.info(f">>>> Stage {STAGE_NAME} completed <<<< \n\nX=====X")
+    logger.info(f">>>> stage {STAGE_NAME} Started <<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STAGE_NAME} completed <<<< \n\nX=====X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+       
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
